6f55f0cfb553fd67a58deef0358f855a89de9b1e
tamaratosic98 <tosict98@gmail.com>
2021-06-29 08:51:51 +0200

    Dokumentovana klasa IOpstiDomenskiObjekat

--- src/main/java/rs/ac/bg/fon/nprog/domen/IOpstiDomenskiObjekat.java
+++ src/main/java/rs/ac/bg/fon/nprog/domen/IOpstiDomenskiObjekat.java
@@ -11,33 +11,81 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
- *
+ * Interfejs koji predstavlja opsti domenski objekat.
  * @author tamara
  */
 public interface IOpstiDomenskiObjekat extends Serializable  {
+	/**
+	 * Vraca naziv tabele iz baze.
+	 *
+	 * @return Naziv tabele kao String.
+	 */
     
     public String nazivTabele();
+    /**
+     * Vraca alijas tabele.
+     * 
+     * @return  Alias kao String.
+     */
     
     public String alijas();
+    /**
+     * Vraca join deo upita.
+     * 
+     * @return Join kao String.
+     */
     
-    public String join();//return "JOIN tabela on...
+    public String join();
+    /**
+     * Vraca where uslov upita.
+     * 
+     * @return Where uslov kao String.
+     */
     
-    public String selectWhere();//vraca where uslov za select
-    //pretraga po nekim stvarima
-    //if username nije null vrati where username='tom sto je dato'
-    //ako je null vrati prazan string
+    public String selectWhere();
+    /**
+     * Vraca listu domenskih objekata iz baze kao rezultat prethodno postavljenog upita.
+     * 
+     * @param rs Podaci iz baze kao ResultSet vrednost.
+     * @return List<IOpstiDomenskiObjekat> kao List<IOpstiDomenskiObjekat>
+     * @throws SQLException ako je rs null.
+     */
     
-    public List<IOpstiDomenskiObjekat> ucitajListu(ResultSet rs) throws SQLException;//parsira resultset u list
+    public List<IOpstiDomenskiObjekat> ucitajListu(ResultSet rs) throws SQLException;
+    /**
+     * Vraca kolone u koje treba insertovati vrednosti.
+     * 
+     * @return Naziv kolona kao String.
+     */
     
-    public String koloneInsert();//ako hocemo da zanemarimo neke vr
+    public String koloneInsert();
+    /**
+     * Vraca vrednost primarnog kljuca tabele.
+     * 
+     * @return Primarni kljuc kao String
+     */
     
-    public String vrednostPrimarniKljuc();//vraca sta je primary key
+    public String vrednostPrimarniKljuc();
+    /**
+     * Vraca vrednosti koje treba ubaciti u tabelu.
+     * 
+     * @return Vrednost za insert kao String.
+     */
     
-    public String vrednostiInsert();//sta ubacujemo
+    public String vrednostiInsert();
+    /**
+     * Vraca vrednosti koje treba izmeniti u table.
+     * 
+     * @return Vrednosti za update kao String.
+     */
     
     public String vrednostiUpdate();
+    /**
+     * Vraca vrednost najveceg primarnog kljuca iz tabele.
+     * 
+     * @return Vrednost najveceg primarnog kljuca.
+     */
     
     public String vratiMax();
-    //zahtev join sa rasom lokacijom
     
 }
