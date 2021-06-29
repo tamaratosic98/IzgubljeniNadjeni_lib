fac32abf6bc5d83e9ec6a7ea9e87e39c46e1a90a
tamaratosic98 <tosict98@gmail.com>
2021-06-29 10:43:29 +0200

    Kreirana pocetna test klasa za Korisnika

---
+++ src/test/java/rs/ac/bg/fon/nprog/domen/KorisnikTest.java
@@ -0,0 +1,59 @@
+package rs.ac.bg.fon.nprog.domen;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class KorisnikTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testKorisnik() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKorisnikLongStringStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetKorisnikId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIme() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrezime() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTelefon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLozinka() {
+		fail("Not yet implemented");
+	}
+
+}
